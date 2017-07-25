3fe599f28852395f23e8a3b58762fffd5cba5265
TK-Works <kim.testa@programmer.net>
2017-07-25 15:58:19 +0800

    Updated assets, config and links

--- application/views/content/index.php
+++ application/views/content/index.php
@@ -1,98 +1,87 @@
 <!DOCTYPE html>
 <head>
-<title>JO n Jud8 Salon</title>
-<meta charset="utf-8">
+    <title>JO n Jud8 Salon</title>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Sacramento|Berkshire+Swash|Courgette|Delius+Swash+Caps|IM+Fell+Great+Primer+SC|Overlock+SC|Bitter|Yrsa" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/font-styles.css'); ?>" media="screen, projection" />
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" media="screen, projection" />
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>" media="screen, projection" />
 </head>
 <body>
- <!-- this is the navbar.. -->
+    <!-- this is the navbar.. -->
     <div id="navbar">
         <nav class="navbar navbar-default navbar-static-top" role="navigation">
             
-        <div class="collapse navbar-collapse" id="navbar-collapse-">
-            <ul class="nav navbar-nav">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About Us</a></li> 
-            <li><a href="services.html">Services</a></li>  
-            <li><a href="reservation.html">Reservations</a></li>
-            <li><a href="gallery.html">Gallery</a></li>
-            <li style="float:right;"><a id= acc1 href="signup.php">Sign Up</a></li>
-            <li style="float:right;"><a id= acc2 href="login.html">Log-in</a></li>
+            <div class="collapse navbar-collapse" id="navbar-collapse-">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="reservation.html">Reservations</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                    <li style="float:right;"><a id= acc1 href="signup.php">Sign Up</a></li>
+                    <li style="float:right;"><a id= acc2 href="login.html">Log-in</a></li>
                 </ul>
             </div>
         </nav>
     </div>
-<img id=banner src="welcomeimg2.jpg">
- 
-        
-        <h2 id="test2"><strong>Get Your Stylish Look Today</strong></h2>
-     
-        <p id="p2">We at JO n Jud8 Salon believe that a strong psychological link exists between looking your best and feeling confident about yourself. JO n Jud8 Salon offers a complete salon services for men and women to pamper the body and spirit.</p>
-          
-            <br>
-            <a id="button1" href=reservation.html><button id="button2" type="button" class="btn btn-primary btn-lg pull-center">Make an Appointment </button></a>
+    <img id=banner src="welcomeimg2.jpg">
+    
+    
+    <h2 id="test2"><strong>Get Your Stylish Look Today</strong></h2>
+    
+    <p id="p2">We at JO n Jud8 Salon believe that a strong psychological link exists between looking your best and feeling confident about yourself. JO n Jud8 Salon offers a complete salon services for men and women to pamper the body and spirit.</p>
+    
+    <br>
+    <a id="button1" href=reservation.html><button id="button2" type="button" class="btn btn-primary btn-lg pull-center">Make an Appointment </button></a>
     <div class=popservices>
         <br>
         <h2 id=popser>Popular Services</h2>
-    <img id= s1 src="s1.png">
-    <img id= s2 src="s2.jpg">
-    <img id= france src="france1.png">
-    <img id= s3 src="s3.jpg">
+        <img id= s1 src="s1.png">
+        <img id= s2 src="s2.jpg">
+        <img id= france src="france1.png">
+        <img id= s3 src="s3.jpg">
     </div>
     
-<div class="welcome">
-    <br>
-    <h2 id=choose><b>Welcome to JO n Jud8 Salon</b></h2>
-    <p id="p4"> JO n Jud8 Salon has a make-up artists and hairstylists with a growing list of happy, beautiful brides, and clients on our portfolio. With a meticulous eye for detail, we use make-up to bring out the client’s best features and make sure that the result is always a clean sophisticated look.With professional team making your look stand out in everyday life. We take care for your health and look. Preparing for an important occasion? JO n Jud8 Salon is the look for you.</p>
-</div>    
+    <div class="welcome">
+        <br>
+        <h2 id=choose><b>Welcome to JO n Jud8 Salon</b></h2>
+        <p id="p4"> JO n Jud8 Salon has a make-up artists and hairstylists with a growing list of happy, beautiful brides, and clients on our portfolio. With a meticulous eye for detail, we use make-up to bring out the client’s best features and make sure that the result is always a clean sophisticated look.With professional team making your look stand out in everyday life. We take care for your health and look. Preparing for an important occasion? JO n Jud8 Salon is the look for you.</p>
+    </div>
     
     <p id="b1">
-    <img src="lip.png"><br>
+        <img src="lip.png"><br>
         <b>Best Stylist</b><br>
-    We have the best stylists to
-    bring out the best feautures in every client to boost their confidence.
-    <a id="button3" href="about.html"><button id="button4" type="button" class="btn btn-primary btn-lg pull-center">Learn More </button></a>
+        We have the best stylists to
+        bring out the best feautures in every client to boost their confidence.
+        <a id="button3" href="about.html"><button id="button4" type="button" class="btn btn-primary btn-lg pull-center">Learn More </button></a>
     </p>
     
     <p id=b2>
         <img src="email.png"><br>
-    <b>Contact</b><br>
-    JOnJud8@gmail.com<br>
-    Fb.com/JOnJud8Salon<br>
-    0917-605-2314<br>
-    <b>Location</b> <br>
-    18 Don Placido Campos
-    St. Brgy Zone 1
+        <b>Contact</b><br>
+        JOnJud8@gmail.com<br>
+        Fb.com/JOnJud8Salon<br>
+        0917-605-2314<br>
+        <b>Location</b> <br>
+        18 Don Placido Campos
+        St. Brgy Zone 1
     </p>
     
     <p id=b3>
         <img src="clock.png"><br>
-    <b>Store Hours</b><br>
-    Mon & Tue: 9:00am to 7:00pm<br>
-    Wed & Thu: 10:00am to 8:00pm<br>
-    Fri & Sat: 9:00am to 9:00pm<br>
-
+        <b>Store Hours</b><br>
+        Mon &amp; Tue: 9:00am to 7:00pm<br>
+        Wed &amp; Thu: 10:00am to 8:00pm<br>
+        Fri &amp; Sat: 9:00am to 9:00pm<br>
     </p>
-<p id=b4>
- <img src="award.png"><br>
+    <p id=b4>
+        <img src="award.png"><br>
         <b>#1 Salon</b><br>
-    We were the champion in the HMAU competition of Hair Asia and won multiple awards.
-    <a id="button5" href=/APPOINTMENT%20SYSTEM/E/about.html><button id="button6" type="button" class="btn btn-primary btn-lg pull-center">Learn More </button></a>
-    </p>  
-
-
-
-
-
-
-
-
-
-
+        We were the champion in the HMAU competition of Hair Asia and won multiple awards.
+        <a id="button5" href=/APPOINTMENT%20SYSTEM/E/about.html><button id="button6" type="button" class="btn btn-primary btn-lg pull-center">Learn More </button></a>
+    </p>
 </body>
+<script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js'); ?>" type="text/javascript"></script>
+<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>" type="text/javascript"></script>
 </html>
