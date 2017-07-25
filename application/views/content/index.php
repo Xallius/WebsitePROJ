81a29762c1969094d91631bfe327f7f570632209
TK-Works <kim.testa@programmer.net>
2017-07-25 16:19:39 +0800

    Ayyyyyyyyyyyy

--- application/views/content/index.php
+++ application/views/content/index.php
@@ -2,14 +2,14 @@
 <h2 id="test2"><strong>Get Your Stylish Look Today</strong></h2>
 <p id="p2">We at JO n Jud8 Salon believe that a strong psychological link exists between looking your best and feeling confident about yourself. JO n Jud8 Salon offers a complete salon services for men and women to pamper the body and spirit.</p>
 <br>
-<a id="button1" href=reservation.html><button id="button2" type="button" class="btn btn-primary btn-lg pull-center">Make an Appointment </button></a>
+<a id="button1" href="<?php echo base_url('index.php/services'); ?>"><button id="button2" type="button" class="btn btn-primary btn-lg pull-center">Make an Appointment </button></a>
 <div class=popservices>
     <br>
     <h2 id=popser>Popular Services</h2>
-    <img id= s1 src="s1.png">
-    <img id= s2 src="s2.jpg">
-    <img id= france src="france1.png">
-    <img id= s3 src="s3.jpg">
+    <img id= s1 src="<?php echo base_url('assets/img/s1.png'); ?>">
+    <img id= s2 src="<?php echo base_url('assets/img/s2.jpg'); ?>">
+    <img id= france src="<?php echo base_url('assets/img/france1.png'); ?>">
+    <img id= s3 src="<?php echo base_url('assets/img/s3.jpg'); ?>">
 </div>
 <div class="welcome">
     <br>
@@ -17,11 +17,11 @@
     <p id="p4"> JO n Jud8 Salon has a make-up artists and hairstylists with a growing list of happy, beautiful brides, and clients on our portfolio. With a meticulous eye for detail, we use make-up to bring out the client’s best features and make sure that the result is always a clean sophisticated look.With professional team making your look stand out in everyday life. We take care for your health and look. Preparing for an important occasion? JO n Jud8 Salon is the look for you.</p>
 </div>
 <p id="b1">
-    <img src="lip.png"><br>
+    <img src="<?php echo base_url('assets/img/lip.png'); ?>"><br>
     <b>Best Stylist</b><br>
     We have the best stylists to
     bring out the best feautures in every client to boost their confidence.
-    <a id="button3" href="about.html"><button id="button4" type="button" class="btn btn-primary btn-lg pull-center">Learn More </button></a>
+    <a id="button3" href="<?php echo base_url('index.php/about'); ?>"><button id="button4" type="button" class="btn btn-primary btn-lg pull-center">Learn More </button></a>
 </p>
 <p id=b2>
     <img src="email.png"><br>
--- application/views/master/layout.php
+++ application/views/master/layout.php
@@ -1,7 +1,7 @@
 <?php 
-	$this->load->view('payslip/template/header');
+	$this->load->view('template/header');
 	
 	$this->load->view($content);
 
-	$this->load->view('payslip/template/scripts');
+	$this->load->view('template/scripts');
 ?>
