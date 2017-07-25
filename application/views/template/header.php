28247fe52d7ff320b28b54b961c2d9fd022c53ad
Xallius <areolaalexanderpaul@gmail.com>
2017-07-25 14:57:07 +0800

    second commit

---
+++ application/views/template/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<head>
+<title>JO n Jud8 Salon</title>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Sacramento|Berkshire+Swash|Courgette|Delius+Swash+Caps|IM+Fell+Great+Primer+SC|Overlock+SC|Bitter|Yrsa" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+     <style>
+       #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
+</head>
+<body>
+
+
+
+ <!-- this is the navbar.. -->
+    <div id="navbar">
+        <nav class="navbar navbar-default navbar-static-top" role="navigation">
+            
+        <div class="collapse navbar-collapse" id="navbar-collapse-1">
+            <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About Us</a></li> 
+            <li><a href="services.php">Services</a></li>  
+            <li><a href="/APPOINTMENT%20SYSTEM/reservation.html">Reservations</a></li>
+            <li><a href="gallery.html">Gallery</a></li>
+            <li style="float:right;"><a id= acc1 href="signup.php">Sign Up</a></li>
+            <li style="float:right;"><a id= acc2 href="login.html">Log-in</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    
+</html>
+</body>
