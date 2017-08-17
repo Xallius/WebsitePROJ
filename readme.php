b7d5da2151da4478df3e3a16b90af65d8bf44783
Xallius <areolaalexanderpaul@gmail.com>
2017-08-17 20:03:50 +0800

    added todo list

---
+++ readme.php
@@ -0,0 +1,5 @@
+// @todo: time alloted for stylist (cannot submit reservation if time requested is already filled out)
+// @todo: services should be categorized if "Male" or "Female", only show "Male" services for male accounts and vice-versa
+// @todo: cannot duplicate services (e.g: 2 rebond sessions is not allowed!!)
+// @todo: have option to tell admin how many people are gonna be served in reservation
+// @todo: dashboard for inquiries (probably questions from customers, messages?), appointment and notifications from appointments
